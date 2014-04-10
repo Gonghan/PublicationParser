303bac624f9942025cd2890bb04f398d0366bf6e
Gonghan <wanggonghan@gmail.com>
2014-04-09 23:26:17 -0700

    Give a formatted sample
    Shelton:
    Give a formatted sample to help user understand long statement.

--- src/com/gonghan/model/SQL_Operations.java
+++ src/com/gonghan/model/SQL_Operations.java
@@ -1,5 +1,14 @@
 package com.gonghan.model;
 
 public interface SQL_Operations {
+	/**
+	 * One sample(formatted): 
+	 * SELECT * FROM soc.articles a
+	 * JOIN soc.coauthors c 
+	 * ON a.id = c.aid 
+	 * WHERE c.authors ='JohnW'
+	 * 
+	 * Get information about all articles written by one author called 'JohnW'
+	 */
 	public static final String GetArticlesFromOneAuthorName="SELECT * FROM soc.articles a JOIN soc.coauthors c ON a.id = c.aid WHERE c.authors = '%s'";
 }
